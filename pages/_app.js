4a1ecc85c867fbfbf9e2f3629b9f1521071ecb67
odunayo12 <odun.rotimi@gmail.com>
2020-09-12 15:14:08 +0200

    initial commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,25 @@
-import '../styles/globals.css'
+// import "../styles/globals.css";
+// #we could use this because we configured it in the next.config.js file else we would have used "../components/Header"
+import Header from "components/Header";
+// import next from "next";
+import { ThemeProvider as TP } from "emotion-theming";
+
+const theme = {
+  colors: {
+    primary: "#ff0000",
+  },
+};
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    // "use empty tag instead of div"
+    <>
+      <TP theme={theme}>
+        <Header />
+        <Component {...pageProps} />
+      </TP>
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
---
+++ pages/about.js
@@ -0,0 +1,6 @@
+// create new route
+const About = () => {
+  return "I am the about page!";
+};
+
+export default About;
