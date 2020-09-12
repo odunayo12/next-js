4f2bf03be6bdbeb8367c9e7ce4f12932b83b5e88
odunayo12 <odun.rotimi@gmail.com>
2020-09-12 18:45:02 +0200

    update css

--- components/Header.js
+++ components/Header.js
@@ -1,5 +1,41 @@
-const Header = () => {
-  return <header>Now I am a real Header!</header>;
+import styled from "@emotion/styled";
+import { rem } from "polished";
+
+// we define our props in the header fxtn and send it to the css so the css has access to it and we can edit it sutably from there
+const Header = ({ isDark }) => {
+  return (
+    <HeaderStyled isDark={isDark}>
+      <div className="container">
+        <div className="logo"></div>
+        <img src="vercel.svg" alt="Site Logo" />
+        <span className=""></span>
+        <span className="logo-text">Next Web</span>
+      </div>
+      Now I am a real Header!
+    </HeaderStyled>
+  );
 };
+
+//header below can be div span ... whatever the root tag in our fuction above is
+const HeaderStyled = styled.header`
+  ${"" /* background: #efefef; */}
+  ${"" /* same as below */}
+  /* background: ${(props) =>
+    props.theme.colors.primary}; */
+  background: ${(props) =>
+    props.isDark ? "#000000" : "#efefef"};
+  padding: 20px;
+  .logo {
+    /* logo is a div className under Header therefore we can indent it here */
+    display: flex;
+    align-items: center;
+    .logo-text {
+      color: #333333;
+      font-weight: bold;
+      font-size: ${rem(20)};
+      margin-left: 20px;
+    }
+  }
+`;
 
 export default Header;
--- pages/_app.js
+++ pages/_app.js
@@ -3,10 +3,11 @@
 import Header from "components/Header";
 // import next from "next";
 import { ThemeProvider as TP } from "emotion-theming";
+import GlobalStyles from "../components/GlobalStyles/GlobalStyles";
 
 const theme = {
   colors: {
-    primary: "#ff0000",
+    primary: "#efefef",
   },
 };
 
@@ -15,6 +16,9 @@ function MyApp({ Component, pageProps }) {
     // "use empty tag instead of div"
     <>
       <TP theme={theme}>
+        <GlobalStyles />
+        {/* isDark is a prop in Header.js */}
+        {/* remove isDark to change to light color */}
         <Header />
         <Component {...pageProps} />
       </TP>
